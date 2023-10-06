1d2b90fd90b1ceb8916274f90da346cb09740fed
jonathanChabert <jonathan.c@valnetinc.com>
2023-10-05 22:14:18 -0400

    adding tinyMCE feature for website

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,10 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6
 {
+    public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
@@ -20,6 +24,16 @@ class ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -29,6 +43,7 @@ class ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit7c1fe5d938c752f5ab56abc04f0989d6::$classMap;
 
         }, null, ClassLoader::class);
